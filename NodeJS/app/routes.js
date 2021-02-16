dea9d0f8923e646cbccdc73fc28b0b869a88e7f5
Ariel <ari.atar1997@gmail.com>
2021-02-15 23:55:34 -0300

    minor changes

--- NodeJS/app/routes.js
+++ NodeJS/app/routes.js
@@ -17,7 +17,7 @@ exports.init = app => {
   app.get('/periodic-news', getPeriodicNews);
   app.get('/medios', getSources);
   // Resuelto: Cambiar estructura modelos sacando feeds, agregando mas medios
-  // TODO: Armar un swagger para la documentacion
+  // Resuelto: Armar un swagger para la documentacion
   // TODO: Modificar documentacion final para entregar
   // TODO: LO HACE OLIVER. Armar en observable algunos graficos para verificar el correcto funcionamiento de la API. Para cada endpoint poner un par de visualizaciones
   app.get('/cantidad-de-noticias', getNewsQuantitySQL);
