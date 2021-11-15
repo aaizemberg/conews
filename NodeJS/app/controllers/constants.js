10142200b4122f95cd33e88824744f664e360a3f
Ariel Aizemberg <ariel.aizemberg@gmail.com>
2021-11-15 17:02:03 -0300

    Update constants.js

--- NodeJS/app/controllers/constants.js
+++ NodeJS/app/controllers/constants.js
@@ -1,97 +1,97 @@
 /* eslint-disable max-lines */
 exports.SOURCES = [
   {
-    id: 0,
+    id: 1,
     name: 'Clarin',
     url: 'https://www.clarin.com'
   },
   {
-    id: 1,
+    id: 2,
     name: 'Infobae',
     url: 'https://www.infobae.com'
   },
   {
-    id: 2,
+    id: 103,
     name: 'La Nacion',
     url: 'https://www.lanacion.com.ar'
   },
   {
-    id: 3,
+    id: 4,
     name: 'Pagina12',
     url: 'https://www.pagina12.com.ar'
   },
   {
-    id: 4,
+    id: 5,
     name: 'Ambito',
     url: 'https://www.ambito.com'
   },
   {
-    id: 5,
+    id: 6,
     name: 'TN',
     url: 'https://www.tn.com.ar'
   },
   {
-    id: 6,
+    id: 7,
     name: 'Perfil',
     url: 'https://www.perfil.com'
   },
   {
-    id: 7,
+    id: 8,
     name: 'Cronista',
     url: 'https://www.cronista.com'
   },
   {
-    id: 8,
+    id: 9,
     name: 'MDZOL',
     url: 'https://www.mdzol.com'
   },
   {
-    id: 9,
+    id: 10,
     name: 'La Voz',
     url: 'https://www.lavoz.com.ar'
   },
   {
-    id: 10,
+    id: 11,
     name: 'Los Andes',
     url: 'https://www.losandes.com.ar'
   },
   {
-    id: 11,
+    id: 12,
     name: 'Destape Web',
     url: 'https://www.eldestapeweb.com'
   },
   {
-    id: 12,
+    id: 13,
     name: 'Diario Uno',
     url: 'https://www.diariouno.com.ar'
   },
   {
-    id: 13,
+    id: 14,
     name: 'BAE Negocios',
     url: 'https://www.baenegocios.com'
   },
   {
-    id: 14,
+    id: 15,
     name: 'Cadena 3',
     url: 'https://www.cadena3.com'
   },
   {
-    id: 15,
+    id: 16,
     name: 'A24',
     url: 'https://www.a24.com'
   },
   {
-    id: 16,
+    id: 17,
     name: 'Cronica',
     url: 'https://www.cronica.com.ar'
   },
   {
-    id: 17,
+    id: 105,
     name: 'El diario AR',
     url: 'https://www.eldiarioar.com'
   }
 ];
 
-exports.DEFAULT_ARRAY = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+exports.DEFAULT_ARRAY = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 103, 105];
 
 exports.DEFAULT_TYPES_ENTITIES = ['MISC', 'PER', 'LOC', 'ORG'];
